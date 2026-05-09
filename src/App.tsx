8fbcc3f69a6382f308a7702996042426d43b82aa
Felipe Moura <felipemimoura@gmail.com>
2026-05-09 06:55:33 -0300

    feat: add struct to content

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,31 @@
-import { Heading } from './componentes/Heading';
-
-import './styles/theme.css';
 import './styles/global.css';
-import { TimerIcon } from 'lucide-react';
+import './styles/theme.css';
 
 function App() {
   return (
     <>
-      <Heading>
-        Olá Mundo
-        <button>
-          <TimerIcon />
-        </button>
-      </Heading>
+      <div className='container'>
+        <div className='content'>
+          <section>LOGO</section>
+        </div>
+      </div>
+      <div className='container'>
+        <div className='content'>
+          <section>LOGO</section>
+        </div>
+      </div>
+
+      <div className='container'>
+        <div className='content'>
+          <section>FORM</section>
+        </div>
+      </div>
+
+      <div className='container'>
+        <div className='content'>
+          <section>FOOTER</section>
+        </div>
+      </div>
     </>
   );
 }
